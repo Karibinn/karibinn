1e0954dbfcb3184687f82f1cfd8cc71f2c4847c4
Kacper Madej <kacperoza@gmail.com>
2018-08-28 16:13:24 +0200

    Refresh admin layout

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Admin
-  class ApplicationController < ::ApplicationController
+  class AdminController < ::ApplicationController
     layout 'admin'
     before_action :authenticate!
 
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Admin
-  class ProductsController < ApplicationController
+  class ProductsController < AdminController
     def index
       @products = Product.all
     end
