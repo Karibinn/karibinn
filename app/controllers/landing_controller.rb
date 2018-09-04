e6e13ca7ef95d703d608b5851830d79391cf8af7
Kacper Madej <kacperoza@gmail.com>
2018-09-04 17:08:24 +0200

    Refactor products spacer partial. TODO: change class names

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,8 +4,8 @@ class LandingController < ApplicationController
   LIMIT = 5
 
   def index
-    @picks = Product.all.eager_load(:category).order('random()').limit(LIMIT)
-    @properties = Product.properties.eager_load(:category).order('random()').limit(LIMIT)
-    @experiences = Product.activities.eager_load(:category).order('random()').limit(LIMIT)
+    @picks = Product.all.eager_load(:category, :images).order('random()').limit(LIMIT)
+    @properties = Product.properties.eager_load(:category, :images).order('random()').limit(LIMIT)
+    @experiences = Product.activities.eager_load(:category, :images).order('random()').limit(LIMIT)
   end
 end
