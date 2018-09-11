c362c9c95800dc79ed02c6dc23f136d03c4167ec
Kacper Madej <kacperoza@gmail.com>
2018-09-11 11:34:44 +0200

    Fix build

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -16,16 +16,4 @@ environment.plugins.append(
   })
 );
 
-environment.loaders.get("file").append(
-  {
-    loader: "url-loader",
-    options: {
-      limit: 8000, // Convert images < 8kb to base64 strings
-      name: "images/[hash]-[name].[ext]"
-    }
-  }
-);
-
 module.exports = environment;
-
-console.log(environment.loaders.get("file"));
