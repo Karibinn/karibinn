80bf865621eeea0e5b0e7e9d55e77a00fe187a37
Kacper Madej <kacperoza@gmail.com>
2018-09-12 16:33:33 +0200

    Use Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,12 +95,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: ENV['MAILGUN_SMTP_SERVER'],
-    port: ENV['MAILGUN_SMTP_PORT'],
-    domain: ENV.fetch('APPLICATION_HOST'),
-    user_name: ENV['MAILGUN_SMTP_LOGIN'],
-    password: ENV['MAILGUN_SMTP_PASSWORD'],
-    authentication: 'plain'
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'karibinn.fr',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
   }
 
   config.action_mailer.default_url_options = { host: ENV.fetch('APPLICATION_HOST') }
