a401782b05a1ca7b515c400638019d824984203d
Kacper Madej <kacperoza@gmail.com>
2018-09-13 18:58:29 +0200

    WIP: Search pills

--- app/javascript/controllers/date_range_picker_controller.js
+++ app/javascript/controllers/date_range_picker_controller.js
@@ -9,22 +9,38 @@
 
 import { Controller } from "stimulus";
 
-import moment from 'moment';
+import moment from "moment";
 
 export default class extends Controller {
   static targets = ["input"];
 
   connect() {
-    $(this.inputTarget).daterangepicker({
-        startDate: moment().startOf('day'),
-        endDate: moment().startOf('day').add(1, 'week'),
-        minYear: new Date().getFullYear(),
-        opens: 'left',
-        autoApply: true,
+    let target = this.inputTarget;
+    let params = {
+      startDate: moment().startOf("day"),
+      endDate: moment().startOf("day").add(1, "week"),
+      minYear: new Date().getFullYear(),
+      opens: target.getAttribute('data-opens') || "left",
+      // autoApply: true,
       locale: {
-          format: 'DD/MM/YYYY'
+        format: "DD/MM/YYYY",
+        cancelLabel: "Clear"
       }
+    };
+
+    if (!!target.getAttribute("data-auto-apply")) {
+      params.autoUpdateInput = false;
+      $(target).daterangepicker(params);
+
+      $(target).on("apply.daterangepicker", function (ev, picker) {
+        $(this).val(picker.startDate.format("MM/DD/YYYY") + " - " + picker.endDate.format("MM/DD/YYYY"));
+      });
+
+      $(target).on("cancel.daterangepicker", function (ev, picker) {
+        $(this).val("");
+      });
+    } else {
+      $(target).daterangepicker(params);
     }
-    )
   }
 }
