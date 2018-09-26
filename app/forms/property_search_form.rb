e43c36bd2c515d87d162ee1656a54dd45660dd6a
Kacper Madej <kacperoza@gmail.com>
2018-09-26 13:27:02 +0200

    Update search pills to include search phrase

--- app/forms/property_search_form.rb
+++ app/forms/property_search_form.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 class PropertySearchForm
+  ATTRIBUTES = %i[guests home_type dates location_slug page per_page phrase].freeze
+
   include ActiveModel::Model
 
-  attr_accessor :guests, :home_type, :dates, :location_slug, :page, :per_page, :phrase
+  attr_accessor *ATTRIBUTES
 
   alias_attribute :category_id, :home_type
 
   # TODO: search by available dates
+  def filled?
+    ATTRIBUTES.any? do |attr|
+      public_send(attr).present?
+    end
+  end
 end
