bc4cede2fba4e8f20dbdcd275679fa8aa4ee1fe5
Kacper Madej <kacperoza@gmail.com>
2018-09-27 15:28:58 +0200

    Make carousel better

--- app/javascript/controllers/image_carousel_controller.js
+++ app/javascript/controllers/image_carousel_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   static targets = ["carousel", "item"];
 
   connect() {
-    $(this.carouselTarget).on('click', 'img', function () {
+    $(this.carouselTarget).on('click', '.carousel-item img', function () {
       window.location = this.src;
     });
 
