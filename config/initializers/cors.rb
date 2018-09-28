f7cb975d81ded3587e4894b493f7871855d64b10
Kacper Madej <kacperoza@gmail.com>
2018-09-28 20:08:26 +0200

    Add CORS for packs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,5 +6,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '/assets/*',
              headers: :any,
              methods: [:get]
+    resource '/packs/*',
+             headers: :any,
+             methods: [:get]
   end
 end
