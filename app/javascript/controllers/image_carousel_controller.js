9213c1087bf9a952c6ab7531cfa36a018ea29e34
Kacper Madej <kacperoza@gmail.com>
2018-10-16 14:58:24 +0200

    Image carousel shows only one image

--- app/javascript/controllers/image_carousel_controller.js
+++ app/javascript/controllers/image_carousel_controller.js
@@ -8,22 +8,5 @@ export default class extends Controller {
     $(this.carouselTarget).on('click', '.carousel-item img', function () {
       window.location = this.src;
     });
-
-    $(this.itemTargets).each(function(){
-      var next = $(this).next();
-      if (!next.length) {
-        next = $(this).siblings(':first');
-      }
-      next.children(':first-child').clone().appendTo($(this));
-
-      for (var i=0;i<2;i++) {
-        next=next.next();
-        if (!next.length) {
-          next = $(this).siblings(':first');
-        }
-
-        next.children(':first-child').clone().appendTo($(this));
-      }
-    });
   }
 }
