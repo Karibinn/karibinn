c9d918c9513413d6984f531e80b0b59faf4d18d3
Kacper Madej <kacperoza@gmail.com>
2018-10-18 18:00:08 +0200

    Fix Localeapp config

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -3,5 +3,12 @@ require 'localeapp/rails'
 Localeapp.configure do |config|
   config.api_key = ENV['LOCALEAPP_API_KEY']
   config.sending_environments = []
-  config.polling_environments = []
+  config.polling_environments = [:production]
+  config.poll_interval = 60
+end
+
+if defined? Rails
+  if Rails.env.production?
+    Localeapp::CLI::Pull.new.execute
+  end
 end
